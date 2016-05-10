303616a1e43b996de7fa382461241b83a616cb78
scheah <sebcheah@gmail.com>
2016-05-10 09:57:58 -0700

    Change program start
    Program takes as input the file pathto query file through command line argument OR user input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
@@ -9,14 +11,22 @@ public class Main {
 		// TODO Auto-generated method stub
 //		ANTLRInputStream input = new ANTLRInputStream( "" );
 		try {
-			ANTLRFileStream input = new ANTLRFileStream("testcase/xqueries/simple.txt");
+			String filePath;
+			if (args.length > 0) // command-line argument
+				filePath = args[0];
+			else { // user input on run
+				Scanner reader = new Scanner(System.in);  // Reading from System.in
+				System.out.println("Enter file path to query: ");
+				filePath = reader.next();
+				reader.close();
+			}
+			ANTLRFileStream input = new ANTLRFileStream(filePath);
 	        XqueryLexer lexer = new XqueryLexer(input);
 	
 	        CommonTokenStream tokens = new CommonTokenStream(lexer);
 	
 	        XqueryParser parser = new XqueryParser(tokens);
 	        ParseTree tree = parser.xq(); // begin parsing at rule 'xq'
-	        System.out.println("Visitor:");
 	        EvalVisitor evalByVisitor = new EvalVisitor();
 	        XqueryNodes result = (XqueryNodes) evalByVisitor.visit(tree);
 	        result.printNodes();
