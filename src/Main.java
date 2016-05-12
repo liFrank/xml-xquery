42201c1469a73373cba952a559cbd0550985b77c
scheah <sebcheah@gmail.com>
2016-05-12 11:10:42 -0700

    Show AST

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-
 
 
 public class Main {
@@ -30,6 +34,16 @@ public class Main {
 	        EvalVisitor evalByVisitor = new EvalVisitor();
 	        XqueryNodes result = (XqueryNodes) evalByVisitor.visit(tree);
 	        result.printNodes();
+	        //show AST in GUI
+	        JFrame frame = new JFrame("Antlr AST");
+	        JPanel panel = new JPanel();
+	        TreeViewer viewr = new TreeViewer(Arrays.asList(parser.getRuleNames()),tree);
+	        viewr.setScale(1.5); //scale option
+	        panel.add(viewr);
+	        frame.add(panel);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.pack();
+	        frame.setVisible(true);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
