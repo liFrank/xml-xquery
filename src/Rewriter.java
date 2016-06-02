d9ae4902a5c13d73534f7965619d385be425a5f5
Jialong <jil653@eng.ucsd.edu>
2016-06-02 10:03:44 -0700

    a coarse implementation.

--- src/Rewriter.java
+++ src/Rewriter.java
@@ -1,7 +1,12 @@
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -34,9 +39,11 @@ public class Rewriter {
 		ParseTree returnclause=begin.getChild(2);
 		
 		//The partitions in the map.
-		ArrayList<HashMap<String,String>> partitions=new ArrayList<HashMap<String,String>>();
+		ArrayList<LinkedHashMap<String,String>> partitions=new ArrayList<LinkedHashMap<String,String>>();
 		//from the variable to its partition.
-		HashMap<String,HashMap<String,String>> fromvartopartition=new HashMap<String,HashMap<String,String>>();
+		LinkedHashMap<String,LinkedHashMap<String,String>> fromvartopartition=new LinkedHashMap<String,LinkedHashMap<String,String>>();
+		//inside each partition, there is a where-list.
+		ArrayList<StringBuilder> wherelist=new ArrayList<StringBuilder>();
 		
 		//first let us analysis for clause;
 		for(int i=0;i<forclause.getChildCount();i++)
@@ -49,11 +56,12 @@ public class Rewriter {
 				System.out.println("in :" +begin.getText());
 				if(begin.getText().contains("document") ||begin.getText().contains("doc"))
 				{
-					HashMap<String,String> partition=new HashMap<String,String>();
+					LinkedHashMap<String,String> partition=new LinkedHashMap<String,String>();
 					partition.put(current_element.getText(), begin.getText());
 					partitions.add(partition);
 					fromvartopartition.put(current_element.getText(), partition);
 					System.out.println("begin a partition!:"+partitions.indexOf(partition));
+					wherelist.add(new StringBuilder(" "));
 				}
 				else
 				{
@@ -75,13 +83,177 @@ public class Rewriter {
 		}
 		if(partitions.size()<2)
 			return "";
+		String whereContent=whereclause.getChild(1).getText();
+		System.out.println("String of where:");
+		String [] condition=whereContent.split("and");
+		ArrayList<String> cond=new ArrayList<String>();
+		for(int i=0;i<condition.length;i++)
+		{
+			System.out.println(condition[i]);
+			cond.add(condition[i]);
+		}
+		boolean initial_join_flag=true;
+		String join="";
+		String first="";
+		//cause professor says on piazza, the testcase can either be complete joinize or not.
+		while(partitions.size()>=2)
+		{
+			//in the loop, we do one join-rewriting.
+			ArrayList<String> firstjointarget=new ArrayList<String>();
+			ArrayList<String> secondjointarget=new ArrayList<String>();
+			for(int i=0;i<cond.size();i++)
+			{
+				//System.out.println("!!!"+cond.toString());
+				String currentCond=cond.get(i);
+				String [] condleftandright=currentCond.split("eq|=");
+				LinkedHashMap<String,String> leftp=fromvartopartition.get(condleftandright[0]);
+				LinkedHashMap<String,String> rightp=fromvartopartition.get(condleftandright[1]);
+				if(leftp==rightp||!condleftandright[0].contains("$")||!condleftandright[1].contains("$"))
+				{//cannot be rewrite to join.
+					int partition_index;
+					if(!condleftandright[0].contains("$"))
+					{
+						partition_index=partitions.indexOf(rightp);
+					}
+					else
+					{
+						partition_index=partitions.indexOf(leftp);
+					}
+					wherelist.get(partition_index).append(" and "+ condleftandright[0]+" eq "+condleftandright[1]+" " );
+				}
+				else if(leftp!=rightp && leftp!=null &&rightp!=null)
+				{//the condition that can be join.
+					if(firstjointarget.isEmpty() && secondjointarget.isEmpty())
+					{
+						firstjointarget.add(condleftandright[0].substring(1));
+						secondjointarget.add(condleftandright[1].substring(1));
+						
+					}
+					else if(fromvartopartition.get("$"+firstjointarget.get(0))==leftp && fromvartopartition.get("$"+secondjointarget.get(0))==rightp )
+					{
+						firstjointarget.add(condleftandright[0].substring(1));
+						secondjointarget.add(condleftandright[1].substring(1));
+						
+					}
+					else if(fromvartopartition.get("$"+secondjointarget.get(0))==leftp && fromvartopartition.get("$"+firstjointarget.get(0))==rightp )
+					{
+						secondjointarget.add(condleftandright[0].substring(1));
+						firstjointarget.add(condleftandright[1].substring(1));
+						
+					}
+					else
+						continue;
+				}
+				else
+					continue;
+				cond.remove(currentCond);
+				i--;
+			}
+			//System.out.println("!"+firstjointarget.toString());
+			//System.out.println("!!"+secondjointarget.toString());
+			LinkedHashMap<String,String> partitionleft=fromvartopartition.get("$"+firstjointarget.get(0));
+			LinkedHashMap<String,String> partitionright=fromvartopartition.get("$"+secondjointarget.get(0));
+			
+			if(initial_join_flag)
+			{
+				initial_join_flag=false;
+				first="for ";
+				for(String var:partitionleft.keySet())
+				{
+					first+= var+ " in "+partitionleft.get(var)+",\n";
+				}
+				first=first.substring(0,first.length()-3);
+				first+="\n";
+				StringBuilder where=wherelist.get(partitions.indexOf(partitionleft));
+				System.out.println("StringBuilder"+where.toString());
+				if(!where.toString().equals(" "))
+				{
+					String wherestring="where "+where.substring(5)+"\n";
+					first+=wherestring;
+				}
+				first+="return <tuple> {\n";
+				for(String var:partitionleft.keySet())
+				{
+					first+="<"+var.substring(1)+">{\t"+var+"\t}"+"</"+var.substring(1)+">,";
+				}
+				first=first.substring(0, first.length()-1)+"\n}</tuple>,\n";
+				
+				//wherelist.remove(partitions.indexOf(partitionleft));
+				//partitions.remove(partitionleft);
+			}
+			//first="join( "+first;
+			String second="for ";
+			for(String var:partitionright.keySet())
+			{
+				second+= var+ " in "+partitionright.get(var)+",\n";
+			}
+			second=second.substring(0,second.length()-3);
+			second+="\n";
+			StringBuilder where=wherelist.get(partitions.indexOf(partitionright));
+			System.out.println("StringBuilder"+where.toString());
+			if(!where.toString().equals(" "))
+			{
+				String wherestring="where "+where.substring(5)+"\n";
+				second+=wherestring;
+			}
+			second+="return <tuple> {\n";
+			for(String var:partitionright.keySet())
+			{
+				second+="<"+var.substring(1)+">{\t"+var+"\t}"+"</"+var.substring(1)+">,";
+			}
+			second=second.substring(0, second.length()-1)+"\n}</tuple>,\n";
+			first+=second;
+			first+=firstjointarget.toString();
+			first+=",";
+			first+=secondjointarget.toString();
+			first+=")\n,";
+			
+			join="join( "+first;
+			first=join;
+			//merge the partition on the right to the partition on the left.
+			for(Map.Entry<String,String> m: partitionright.entrySet())
+			{
+				partitionleft.put(m.getKey(), m.getValue());
+				fromvartopartition.replace(m.getKey(), partitionleft);
+			}
+			wherelist.remove(partitions.indexOf(partitionright));
+			partitions.remove(partitionright);
+			System.out.println(first);
+		}
+		result="for $tuple in "+join.substring(0,join.length()-1);
+		//dealing with return clause.
+		String returnclauseString=returnclause.getText();
+		System.out.println(returnclauseString);
+		Pattern pattern1=Pattern.compile("\\$\\w+\\/text\\(\\)");//text()
+		Pattern pattern2=Pattern.compile("\\$\\w+\\/[^\\/]");//not //
+		Pattern pattern3=Pattern.compile("\\$\\w+[,| |\\n|}|\\t]");//nothing after
 		
+		Matcher matcher1=pattern1.matcher(returnclauseString);
+		while(matcher1.find())
+		{
+			String m1=matcher1.group(0).replace("/", "//");
+			returnclauseString=returnclauseString.replace(matcher1.group(0), m1);
+		}
+		Matcher matcher2=pattern2.matcher(returnclauseString);
+		while(matcher2.find())
+		{
+			String m2=matcher2.group(0);
+			returnclauseString=returnclauseString.replace(m2, m2.substring(0, m2.length()-2)+"/*"+m2.substring(m2.length()-2));
+		}
+		Matcher matcher3= pattern3.matcher(returnclauseString);
+		while(matcher3.find())
+		{
+			String m3=matcher3.group(0);
+			returnclauseString=returnclauseString.replace(m3, m3.substring(0, m3.length()-1)+"/*"+m3.substring(m3.length()-1));
+		}
+		
+		result+=returnclauseString.replace("$","$tuple/");
 		
 		if(resultTag!=null)
 		{
 			result = "<" + resultTag + ">  \n{\n" + result + "\n}\n  </" + resultTag + ">";
 		}
-		return "!";
+		return result;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
